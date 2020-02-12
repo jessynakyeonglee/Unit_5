571f8fd47a76aa6b53afeccd3b43fff4b9038579
leena <leena@ssfs.org>
2020-02-12 18:11:19 -0500

    2/12/20

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,26 +1,49 @@
-//Jessy Lee
-//2/11/20
-//This program will perform arithmetic with fractions
+/**@author Jessy Lee
+ * @since 2/11/20
+ * This program will perform arithmetic with fractions
+ */
+
 public class Fraction {
     private int numerator;
     private int denominator;
 
+    /**
+     * The first constroctor will create a fraction that consists of a numerator and a denominator and will store the fraction in a reduced form
+     * @param numerator the numerator of the fraction
+     * @param denominator the denominator of the fraction
+     */
+
     public Fraction(int numerator, int denominator) {
-        this.denominator = denominator;
         this.numerator = numerator;
-        int factor = gcd(this.numerator, this.denominator);
+        this.denominator = denominator;
+        int factor = gcd(numerator, denominator);
         this.numerator /= factor;
         this.denominator /= factor;
-    }
-
-    public void findNegatives() {
-        if (numerator >= 0 && denominator < 0) {
-            numerator -= numerator * 2;
-            denominator += denominator * 2;
+        if ((numerator>0&&denominator<0)||(numerator<0&&denominator<0)){
+            this.numerator=-numerator;
+            this.denominator=-denominator;
         }
     }
 
-    private int gcd(int m, int n) {
+    /**
+     * This will create a default constrctor with both the numerator and the denominator as 1.
+     */
+    public Fraction(){
+        this(1,1);
+    }
+
+    /**
+     * This will find the greatest common multiple of numerator and denominator
+     * @param numerator numerator of the fraction
+     * @param denominator denominator of the fraction
+     * @return the greatest common multiple of numerator and denominator.
+     */
+    private int gcd(int numerator, int denominator) {
+        int m=numerator;
+        int n=denominator;
+        if (m % n == 0) {
+            return n;
+        }
         while (m % n != 0) {
             int oldm = m;
             int oldn = n;
@@ -30,31 +53,68 @@ public class Fraction {
         }
         return n;
     }
-
     /**
-     * This method will do arithmetic s
-     * @param a
-     * @param b
-     * @return
+     * This will add two fractions together and return the result in the reduced form
+     * @param a the first fraction that will be added
+     * @param b the second fraction that will be added
+     * @return the added fraction in the reduced form
      */
     public static Fraction add(Fraction a, Fraction b) {
-        Fraction addedfraction = new Fraction(((a.denominator * b.numerator) + (b.denominator * a.numerator)), (a.denominator) * (b.denominator));
+        int newnumerator =(a.denominator * b.numerator) + (b.denominator * a.numerator);
+        int newdenominator =(a.denominator) * (b.denominator);
+        Fraction addedfraction = new Fraction(newnumerator,newdenominator);
         return addedfraction;
     }
-
+    /**
+     * This will subtract two fractions and return the result in the reduced form
+     * @param a the first fraction that will be subtracted from
+     * @param b second fraction that will subtract by
+     * @return the subtracted fraction in the reduced form
+     */
     public static Fraction subtract(Fraction a, Fraction b) {
-        Fraction subtractedfraction = new Fraction(((a.denominator * b.numerator) - (b.denominator * a.numerator)), (a.denominator) * (b.denominator));
+        int newnumerator =(a.numerator * b.denominator) - (b.numerator * a.denominator);
+        int newdenominator =(a.denominator) * (b.denominator);
+        Fraction subtractedfraction = new Fraction(newnumerator,newdenominator);
         return subtractedfraction;
     }
+
+    /**
+     * This will multiply two fractions together and return the result in the reduced form
+     * @param a the first fraction that will be multplied
+     * @param b the second fraction that will be multiplied
+     * @return the multiplied fraction in the reduced form
+     */
     public static Fraction multiply(Fraction a, Fraction b) {
-        Fraction multipliedfraction = new Fraction((a.numerator)*(b.numerator), (a.denominator) * (b.denominator));
+        int newnumerator =(a.numerator)*(b.numerator);
+        int newdenominator =(a.denominator) * (b.denominator);
+        Fraction multipliedfraction = new Fraction(newnumerator,newdenominator);
         return multipliedfraction;
     }
+    /**
+     * This will divde two fractions and return the result in the reduced form
+     * @param a the first fraction that will be divded by the second fraction
+     * @param b the second fraction that will divide the first fraction
+     * @return the divided fraction in the reduced form
+     */
     public static Fraction divide(Fraction a, Fraction b) {
-        Fraction dividedfraction = new Fraction((a.numerator)*(b.denominator), (a.denominator) * (b.numerator));
+        int newnumerator = (a.numerator)*(b.denominator);
+        int newdenominator = (a.denominator)*(b.numerator);
+        Fraction dividedfraction = new Fraction(newnumerator,newdenominator);
         return dividedfraction;
     }
+
+    /**
+     * this will print the fraction in the reduced form with the fraction sign, "/".
+     * @return the fraction that was resulted by the arithmetics above.
+     */
     public String toString (){
+        int gcd=gcd(numerator,denominator);
+        this.numerator/=gcd;
+        this.denominator/=gcd;
+        if ((numerator>0&&denominator<0)||(numerator<0&&denominator<0)) {
+            this.numerator = -numerator;
+            this.denominator = -denominator;
+        }
         return numerator+"/"+denominator;
     }
 }
